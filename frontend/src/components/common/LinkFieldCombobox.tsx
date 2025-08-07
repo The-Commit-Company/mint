ff37be87a271d67cbbc46a13b76d830ef93ccda7
Nikhil Kothari <nik.kothari22@live.com>
2025-08-07 16:30:15 +0530

    fix: remove value

--- frontend/src/components/common/LinkFieldCombobox.tsx
+++ frontend/src/components/common/LinkFieldCombobox.tsx
@@ -254,7 +254,7 @@ const LinkFieldCombobox = ({
             <PopoverContent className="p-0" style={{ minWidth: width }} align="start">
                 {error && <ErrorBanner error={error} />}
                 <Command shouldFilter={false} className="w-full">
-                    <CommandInput placeholder={_(placeholder)} onValueChange={setSearchInput} value={searchInput} />
+                    <CommandInput placeholder={_(placeholder)} onValueChange={setSearchInput} />
                     <CommandList>
                         <CommandEmpty>{isLoading ? _("Loading...") : _("No results found.")}</CommandEmpty>
                         <CommandGroup>
