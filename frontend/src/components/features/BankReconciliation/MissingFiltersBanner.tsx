4b37f9a3803d62674f0debc1ec442ba3bcbf2898
Nikhil Kothari <nik.kothari22@live.com>
2025-08-08 15:30:37 +0530

    fix: set correct min-height

--- frontend/src/components/features/BankReconciliation/MissingFiltersBanner.tsx
+++ frontend/src/components/features/BankReconciliation/MissingFiltersBanner.tsx
@@ -1,9 +1,10 @@
 import { Paragraph } from "@/components/ui/typography"
+import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 
 
-export const MissingFiltersBanner = ({ text }: { text: ReactNode }) => {
-    return <div className="min-h-[50vh] flex items-center justify-center">
+export const MissingFiltersBanner = ({ text, className }: { text: ReactNode, className?: string }) => {
+    return <div className={cn("min-h-[50vh] flex items-center justify-center", className)}>
         <Paragraph>{text}</Paragraph>
     </div>
 }
