6a0c280b5acd29c0293819e167c72f622f3483b8
Nikhil Kothari <nik.kothari22@live.com>
2025-08-25 11:40:40 -0400

    feat: match transfers across date range (#41)

--- frontend/src/components/features/Settings/Settings.tsx
+++ frontend/src/components/features/Settings/Settings.tsx
@@ -1,16 +1,16 @@
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import ErrorBanner from '@/components/ui/error-banner'
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form } from '@/components/ui/form'
 import { DataField } from '@/components/ui/form-elements'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { getErrorMessage } from '@/lib/frappe'
+// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+// import { getErrorMessage } from '@/lib/frappe'
 import _ from '@/lib/translate'
 import { MintSettings } from '@/types/Mint/MintSettings'
-import { useFrappeGetCall, useFrappeGetDoc, useFrappePostCall, useFrappeUpdateDoc } from 'frappe-react-sdk'
+import { useFrappeGetDoc, useFrappeUpdateDoc } from 'frappe-react-sdk'
 import { SettingsIcon } from 'lucide-react'
 import { useState } from 'react'
-import { useForm, useFormContext } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 
 const Settings = () => {
@@ -25,24 +25,25 @@ const Settings = () => {
                 </Button>
             </DialogTrigger>
             <DialogContent>
-                <SettingsDialogContent />
+                <SettingsDialogContent onClose={() => setIsOpen(false)} />
             </DialogContent>
         </Dialog>
     )
 }
 
-const SettingsDialogContent = () => {
+const SettingsDialogContent = ({ onClose }: { onClose: VoidFunction }) => {
 
     const form = useForm<MintSettings>({
         defaultValues: {
-            google_project_id: "",
-            google_processor_location: "us",
-            google_service_account_json_key: "",
-            bank_statement_gdoc_processor: "",
+            transfer_match_days: 4,
+            // google_project_id: "",
+            // google_processor_location: "us",
+            // google_service_account_json_key: "",
+            // bank_statement_gdoc_processor: "",
         }
     })
 
-    const { data, mutate, error: fetchError } = useFrappeGetDoc<MintSettings>("Mint Settings", "Mint Settings", undefined, {
+    const { mutate, error: fetchError } = useFrappeGetDoc<MintSettings>("Mint Settings", "Mint Settings", undefined, {
         onSuccess: (data) => {
             form.reset(data)
         },
@@ -56,6 +57,7 @@ const SettingsDialogContent = () => {
             .then(() => {
                 toast.success(_("Settings updated"))
                 mutate()
+                onClose()
             })
     }
 
@@ -65,13 +67,26 @@ const SettingsDialogContent = () => {
         <form onSubmit={form.handleSubmit(onSubmit)}>
             <DialogHeader>
                 <DialogTitle>{_("Settings")}</DialogTitle>
-                <DialogDescription>{_("Configure settings for Google Document AI.")}</DialogDescription>
+                <DialogDescription>{_("Configure settings for Mint.")}</DialogDescription>
             </DialogHeader>
             <div className='flex flex-col gap-4 w-full py-4'>
                 {fetchError && <ErrorBanner error={fetchError} />}
                 {error && <ErrorBanner error={error} />}
 
                 <DataField
+                    name='transfer_match_days'
+                    label={"Number of Days to Match Transfers"}
+                    isRequired
+                    inputProps={{
+                        type: 'number',
+                        inputMode: 'numeric',
+                    }}
+                    formDescription='For example, if set to 4, Mint will try to find matching transactions in other banks 4 days before and after the transaction date. This is because transactions can clear on different days on different bank accounts.'
+                />
+
+                {/* <H4 className='text-sm font-semibold'>Google Document AI</H4> */}
+
+                {/* <DataField
                     name='google_project_id'
                     label={"Google Project ID"}
                     isRequired
@@ -111,7 +126,7 @@ const SettingsDialogContent = () => {
                     formDescription='Paste the Service Key (JSON) from the Google Cloud Console.'
                 />
 
-                {data && data.google_project_id && data.google_service_account_json_key && <ProcessorField />}
+                {data && data.google_project_id && data.google_service_account_json_key && <ProcessorField />} */}
 
             </div>
 
@@ -126,65 +141,65 @@ const SettingsDialogContent = () => {
 }
 
 
-const ProcessorField = () => {
+// const ProcessorField = () => {
 
-    const form = useFormContext()
+//     const form = useFormContext()
 
-    const { data, error, mutate } = useFrappeGetCall("mint.apis.google_ai.get_list_of_processors", {
-        processor_type: "BANK_STATEMENT"
-    }, undefined, {
-        revalidateOnFocus: false,
-        revalidateIfStale: false
-    })
+//     const { data, error, mutate } = useFrappeGetCall("mint.apis.google_ai.get_list_of_processors", {
+//         processor_type: "BANK_STATEMENT"
+//     }, undefined, {
+//         revalidateOnFocus: false,
+//         revalidateIfStale: false
+//     })
 
-    const { call: createProcessor } = useFrappePostCall('mint.apis.google_ai.create_document_processor')
+//     const { call: createProcessor } = useFrappePostCall('mint.apis.google_ai.create_document_processor')
 
-    const onProcessorCreate = () => {
-        createProcessor({})
-            .then((data) => {
-                mutate()
-                form.setValue("bank_statement_gdoc_processor", data.message.id)
-            }).catch((error) => {
-                toast.error(_("Failed to create processor"), {
-                    description: getErrorMessage(error)
-                })
-            })
-    }
+//     const onProcessorCreate = () => {
+//         createProcessor({})
+//             .then((data) => {
+//                 mutate()
+//                 form.setValue("bank_statement_gdoc_processor", data.message.id)
+//             }).catch((error) => {
+//                 toast.error(_("Failed to create processor"), {
+//                     description: getErrorMessage(error)
+//                 })
+//             })
+//     }
 
-    return <div>
-        {error && <ErrorBanner error={error} />}
-        {data && <div className='flex flex-col gap-2'>
-            <FormField
-                control={form.control}
-                name={"bank_statement_gdoc_processor"}
-                render={({ field }) => (
-                    <FormItem className='flex flex-col'>
-                        <FormLabel>{_("Bank Statement Processor")}<span className="text-destructive">*</span></FormLabel>
-                        <Select onValueChange={field.onChange} value={field.value}>
-                            <FormControl>
-                                <SelectTrigger className='w-full'>
-                                    <SelectValue placeholder={_("Select Processor")} />
-                                </SelectTrigger>
-                            </FormControl>
-                            <SelectContent>
-                                {data?.message.map((processor: { id: string, display_name: string }) => (
-                                    <SelectItem key={processor.id} value={processor.id}>{processor.display_name} ({processor.id})</SelectItem>
-                                ))}
-                            </SelectContent>
-                        </Select>
-                        <FormDescription>{_("Location where the document processor is run.")}</FormDescription>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
+//     return <div>
+//         {error && <ErrorBanner error={error} />}
+//         {data && <div className='flex flex-col gap-2'>
+//             <FormField
+//                 control={form.control}
+//                 name={"bank_statement_gdoc_processor"}
+//                 render={({ field }) => (
+//                     <FormItem className='flex flex-col'>
+//                         <FormLabel>{_("Bank Statement Processor")}<span className="text-destructive">*</span></FormLabel>
+//                         <Select onValueChange={field.onChange} value={field.value}>
+//                             <FormControl>
+//                                 <SelectTrigger className='w-full'>
+//                                     <SelectValue placeholder={_("Select Processor")} />
+//                                 </SelectTrigger>
+//                             </FormControl>
+//                             <SelectContent>
+//                                 {data?.message.map((processor: { id: string, display_name: string }) => (
+//                                     <SelectItem key={processor.id} value={processor.id}>{processor.display_name} ({processor.id})</SelectItem>
+//                                 ))}
+//                             </SelectContent>
+//                         </Select>
+//                         <FormDescription>{_("Location where the document processor is run.")}</FormDescription>
+//                         <FormMessage />
+//                     </FormItem>
+//                 )}
+//             />
 
-            {data.message.length === 0 && <div>
-                <Button variant={'outline'} type='button' onClick={onProcessorCreate}>{_("Create New Bank Statement Processor")}</Button>
-            </div>}
-        </div>
-        }
-    </div>
+//             {data.message.length === 0 && <div>
+//                 <Button variant={'outline'} type='button' onClick={onProcessorCreate}>{_("Create New Bank Statement Processor")}</Button>
+//             </div>}
+//         </div>
+//         }
+//     </div>
 
-}
+// }
 
 export default Settings
